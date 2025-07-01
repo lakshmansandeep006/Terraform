2cd85c27b9338b18205efa805175fdb3072ff802
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-07-01 20:27:28 +0530

    Auto push

---
+++ expense-v1/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-0ec18f6103c5e0491"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = "sg-0c82156e2901a05c4"
+ 
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "backend" {
+  ami                    = "ami-0ec18f6103c5e0491"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = "sg-0c82156e2901a05c4"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-0ec18f6103c5e0491"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = "sg-0c82156e2901a05c4"
+ 
+  tags = {
+    Name = "mysql"
+  }
+}
